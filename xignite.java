dd1b064b6c4ef4baa86bb328735b76155b04871b
System Administrator <root@Anands-MacBook-Air.local>
2014-03-11 12:54:20 -0700

    hello world

---
+++ xignite.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+ 
+
+ /** 
+ * The Xignite class implements an application that
+ * makes a rest call to 
+ * xigniteGlobalCurrencies ->  ListCurrencies.
+ * It receives Json data from the service.
+ * It then parses the data and displays it to console.
+ **
+ * Documentation for xigniteGlobalCurrencies:
+ * https://www.xignite.com/product/forex/api/ListCurrencies/
+ **
+ * Make sure to include Json library (provided here) in classpath.
+ **
+ * Put your API token into the api token field in config.properties file
+ **/
+
+public class xignite {
+ 
+	public static void main(String[] args) {
+
+		JSONParser parser = new JSONParser();
+ 
+		try {
+		  	Properties configFile = new Properties();
+		  	InputStream input = new FileInputStream("config.properties");
+			configFile.load(input);
+
+	 		String api_token = configFile.getProperty("API_TOKEN");
+	 		System.out.println(api_token);
+
+			URL url = new URL("http://globalcurrencies.xignite.com/xGlobalCurrencies.json/ListCurrencies?_token="+ api_token);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+	 
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+	 
+			String output="";
+			//JSONObject jsonObj = new JSONObject(output);
+			String fuls = "";
+			System.out.println("Output from Server .... \n");
+			
+			while ((output = br.readLine()) != null) {
+				fuls = fuls + output;
+			}
+
+			
+	 		Object obj = parser.parse(fuls);
+	 		JSONObject jsonObject = (JSONObject) obj;
+	 		JSONArray currencies = (JSONArray) jsonObject.get("CurrencyList");
+	 		
+	 		int j = 0;
+
+			while (j < currencies.size()) {
+				System.out.println("\n next:");
+				JSONObject currency = (JSONObject) currencies.get(j);
+				String name = (String) currency.get("Name");
+				String symbol = (String) currency.get("Symbol");
+
+				System.out.println(name);
+				System.out.println(symbol);
+
+				j++;
+		}
+
+			conn.disconnect();
+	 
+		  } catch (MalformedURLException e) {
+	 
+			e.printStackTrace();
+	 
+		  } catch (IOException e) {
+	 
+			e.printStackTrace();
+	 
+		  } catch (ParseException e) {
+			e.printStackTrace();
+		}
+ 
+	}
+ 
+}
